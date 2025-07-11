de177b34dea3e56d7cb1c28b96f2ca9a15ef9e06
Michael Yang <themichaelyang@gmail.com>
2025-07-11 16:54:21 -0700

    add tokenizer tests

--- lisp.rb
+++ lisp.rb
@@ -8,27 +8,30 @@
 
 # Useful reference: https://norvig.com/lispy.html
 
-class AST < Array
-end
+SPACE = " "
+PARENS = ["(", ")"]
+EOF = "EOF"
 
 class Tokenizer
   def initialize(code)
     @tokens = []
     @current_token = []
     @code = code
+
+    @chars = code.chars
+    @chars.append(EOF)
   end
 
   def tokenize
-    @code.chars.each do |char|
+    @chars.each do |char|
       case char
-      when " "
+      when SPACE
         flush_current_token!
-      when "("
-        flush_current_token!
-        add_token!(char)
-      when ")"
+      when *PARENS
         flush_current_token!
         add_token!(char)
+      when EOF
+        flush_current_token!
       else
         @current_token.append(char)
       end
@@ -50,7 +53,60 @@ class Tokenizer
       add_token!(@current_token.join)
       reset_current_token!
     end
-  end 
+  end
 end
 
-puts Tokenizer.new("(+ (+ 1 2) 3)").tokenize.inspect
+class Testing
+  def self.test_tokenizer
+    assert_equals(Tokenizer.new("123").tokenize, ["123"])
+    assert_equals(Tokenizer.new("(+ 1 2)").tokenize, ["(", "+", "1", "2", ")"])
+    assert_equals(Tokenizer.new("(+ (+ 1 2) 34)").tokenize, ["(", "+", "(", "+", "1", "2", ")", "34", ")"])
+    assert_equals(Tokenizer.new('(append "abc" "def")').tokenize, ["(", "append", '"abc"', '"def"', ")"])
+  end
+
+  def self.assert_equals(actual, expected)
+    @@assertion_num += 1
+
+    if actual == expected
+      puts "- Check #{@@assertion_num} passed!"
+      @@assertions_passed += 1
+    else
+      puts "-️ 💥 Check #{@@assertion_num} failed! #{actual} != #{expected}"
+    end
+  end
+
+  def self.test
+    puts "Testing..."
+    puts ''
+
+    self.methods.each do |method|
+      if method.start_with?("test_")
+        reset_state!
+
+        puts "Running #{method}:"
+        self.send(method)
+
+        if @@assertions_passed == @@assertion_num
+          puts "✅ Passed #{method}: #{@@assertion_num - @@assertions_passed} failures + #{@@assertions_passed} passed = #{@@assertion_num} total"
+        else
+          puts "⛔️ Failed #{method}: #{@@assertion_num - @@assertions_passed} failures + #{@@assertions_passed} passed = #{@@assertion_num} total"
+        end
+
+        puts ''
+      end
+    end
+  end
+
+  def self.reset_state!
+    @@assertion_num = 0
+    @@assertions_passed = 0
+  end
+end
+
+def main
+  if ARGV.include? "--test"
+    Testing.test
+  end
+end
+
+main
