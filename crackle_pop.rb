ad7e11e5b3678af9a561cd86198fab4142b101b1
Michael Yang <themichaelyang@gmail.com>
2025-07-11 15:48:55 -0700

    replace "not" and "and"

--- crackle_pop.rb
+++ crackle_pop.rb
@@ -4,49 +4,49 @@
 # If it's divisible by both 3 and 5, print CracklePop instead of the number.
 # You can use any language.
 
+def crackle_pop(range, crackle: 3, pop: 5)
+  range.map do |i|
+    crackled = i.divisible_by? crackle
+    popped = i.divisible_by? pop
+
+    "#{"Crackle" if crackled}#{"Pop" if popped}#{i.to_s if !crackled && !popped}"
+  end
+end
+
 class Integer
   def divisible_by?(divisor)
     self % divisor == 0
   end
 end
 
-def crackle_pop(range, crackle: 3, pop: 5)
-  range.map do |i|
-    crackled = i.divisible_by? crackle
-    popped = i.divisible_by? pop
+class Testing
+  @@test_num = 1
 
-    "#{"Crackle" if crackled}#{"Pop" if popped}#{i.to_s if not crackled and not popped}"
-  end
-end
+  def self.assert_equals(actual, expected)
+    if actual == expected
+      puts "✅ Test #{@@test_num} passed!"
+    else
+      puts "⛔️ Test #{@@test_num} failed! #{actual} != #{expected}"
+    end
 
-@test_num = 1
-
-def assert_equals(actual, expected)
-  if actual == expected
-    puts "✅ Test #{@test_num} passed!"
-  else
-    raise "⛔️ Test failed! #{actual} != #{expected}"
+    @@test_num += 1
   end
 
-  @test_num += 1
-end
-
-def test_crackle_pop
-  assert_equals(crackle_pop([15]), ["CracklePop"])
-  assert_equals(crackle_pop([3]), ["Crackle"])
-  assert_equals(crackle_pop([5]), ["Pop"])
-  assert_equals(crackle_pop([1]), ["1"])
-  assert_equals(crackle_pop([2]), ["2"])
-  assert_equals(crackle_pop([7]), ["7"])
-  assert_equals(
-    crackle_pop(1..15), 
-    ["1", "2", "Crackle", "4", "Pop", "Crackle", "7", "8", "Crackle", "Pop", "11", "Crackle", "13", "14", "CracklePop"]
-  )
+  def self.test_crackle_pop
+    assert_equals(crackle_pop([1, 2, 7]), ["1", "2", "7"])
+    assert_equals(crackle_pop([3]), ["Crackle"])
+    assert_equals(crackle_pop([5]), ["Pop"])
+    assert_equals(crackle_pop([15]), ["CracklePop"])
+    assert_equals(
+      crackle_pop(1..15), 
+      ["1", "2", "Crackle", "4", "Pop", "Crackle", "7", "8", "Crackle", "Pop", "11", "Crackle", "13", "14", "CracklePop"]
+    )
+  end
 end
 
 def main
-  if ARGV.include? "test"
-    test_crackle_pop
+  if ARGV.include? "--test"
+    Testing.test_crackle_pop
   else
     puts crackle_pop(1..100)
   end
